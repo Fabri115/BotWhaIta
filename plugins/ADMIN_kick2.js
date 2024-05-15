c1c4c19f5dcca328e21ffc38379d05c45f319a29
daddebot <168578034+daddebot@users.noreply.github.com>
2024-05-15 21:27:38 +0200

    Update ADMIN_kick2.js

--- plugins/ADMIN_kick2.js
+++ plugins/ADMIN_kick2.js
@@ -8,7 +8,7 @@ const handler = async (m, {conn, participants, usedPrefix, command}) => {
   await conn.groupParticipantsUpdate(m.chat, [user], 'remove');
   m.reply('*[❗𝐈𝐍𝐅𝐎❗]SUPER DADDE HA POLVERIZZATO IL COGLIONE *\@n* —◉ LA VITTIMA NON POTRA PIU DARE FASTIDIO*');
 };
-handler.command = /^(kick2|via|topo)$/i;
+handler.command = /^(kick2|via|topolino)$/i;
 handler.admin = true;
 handler.group = true;
 handler.botAdmin = true;
